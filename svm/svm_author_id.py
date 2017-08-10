299f62f87a1e0179d30e831804542280ab697b9c
amritasha <amritasha71@gmail.com>
2017-08-10 20:03:46 +0530

    svm - accuracy - 0.99

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,25 +26,81 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 from sklearn import svm
+from sklearn.metrics import accuracy_score
+'''
 t0 = time()
 clf = svm.SVC(kernel='linear')
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
-from sklearn.metrics import accuracy_score
+t1 = time()
+print accuracy_score(labels_test, clf.predict(features_test))
+print "testing time:", round(time()-t1, 3), "s"
+'''
+
+'''
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+'''
+
+'''
+print "small dataset"
+t0 = time()
+clf = svm.SVC(kernel='linear')
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
+print accuracy_score(labels_test, clf.predict(features_test))
+print "testing time:", round(time()-t1, 3), "s"
+'''
+
+'''
+print
+print "rbf c=10.0"
+t0 = time()
+clf = svm.SVC(kernel='rbf', C=10.0)
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 t1 = time()
 print accuracy_score(labels_test, clf.predict(features_test))
 print "testing time:", round(time()-t1, 3), "s"
 
-print "small dataset"
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
+print
+print "rbf c=100.0"
 t0 = time()
-clf = svm.SVC(kernel='linear')
+clf = svm.SVC(kernel='rbf', C=100.0)
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
 t1 = time()
 print accuracy_score(labels_test, clf.predict(features_test))
 print "testing time:", round(time()-t1, 3), "s"
+
+print
+print "rbf c=1000.0"
+t0 = time()
+clf = svm.SVC(kernel='rbf', C=1000.0)
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
+print accuracy_score(labels_test, clf.predict(features_test))
+print "testing time:", round(time()-t1, 3), "s"
+'''
+print
+print "rbf c=10000.0"
+t0 = time()
+clf = svm.SVC(kernel='rbf', C=10000.0)
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
+pred = clf.predict(features_test)
+print accuracy_score(labels_test,pred )
+print "testing time:", round(time()-t1, 3), "s"
+print pred[10]
+print pred[26]
+print pred[50]
+
+import numpy as np
+print "no of chris mails predicted : "
+print np.count_nonzero(pred==1)
 #########################################################
 
 
